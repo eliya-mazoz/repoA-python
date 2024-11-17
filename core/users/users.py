f1475e482ff565eba0754514d95e36357bf2500c
eliya-mazoz <104295739+eliya-mazoz@users.noreply.github.com>
2024-11-17 12:39:45 +0200

    Update users.py

--- core/users/users.py
+++ core/users/users.py
@@ -1,11 +1,25 @@
 import asyncio
-from user_dataclasses import User
+import random
 
+from core.users.user_dataclasses import User
 
 class Users:
 
     def __init__(self):
-        pass
+        self.current_id = 0
+        self.users = {}
+
+    async def create_user(self, user_data: User) -> None:
+        """
+        Create user
+        """
+        if user_data.id is None:
+            self.current_id = random.randint(1, 1000)
+            user_data.id = self.current_id
+        if user_data.id in self.users:
+            raise Exception("User already exists")
+        self.users[user_data.id] = user_data
+        print(f"User {user_data.name} with ID {user_data.id} was successfully created ")
 
     async def get_user(self, user_id: int) -> User:
         """
@@ -14,23 +28,17 @@ class Users:
             user_id (int): user id
         """
         print("get user id ", user_id)
-        return User(name='John Doe', username='johndoe', email="test@test.com", age=25)
+        user = self.users.get(user_id)
+        if user is None:
+            raise Exception("User not found")
+        return user
 
-    async def create_user(self, user_data: User) -> None:
-        """
-        Create user
-        """
-        print("successfully created user")
-
-    async def update_user(self, user_id: int, user_data: User) -> None:
-        """
-        Update user
-        """
-
-        print("successfully updated user", user_id)
 
     async def delete_user(self, user_id: int) -> None:
         """
         Delete user
         """
+        user = self.users.pop(user_id)
+        if user is None:
+            raise Exception("User not found")
         print("successfully deleted user", user_id)
